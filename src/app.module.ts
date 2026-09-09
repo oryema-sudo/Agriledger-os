e570c9221777052f542ab959f9497e811ea8099f
oryema-sudo <oryemajoseph3@gmail.com>
2026-09-10 00:47:17 +0300

    Add src/app.module.ts placeholder

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+/**
+ * Root application module – placeholder for Phase 1.
+ *
+ * Future modules:
+ * - ConfigModule
+ * - AuthModule
+ * - OrganizationsModule
+ * - UsersModule
+ * - SuppliersModule
+ * - AgentsModule
+ * - ProcurementModule
+ * - PaymentsModule
+ * - ReconciliationModule
+ * - SalesModule
+ * - DocumentsModule
+ * - AuditModule
+ * - DashboardModule
+ */
+
+export class AppModule {}
